de6694f32888fd3d462d1f6f725b38fa2a25c466
Ahmed Fasih <wuzzyview@gmail.com>
2018-03-05 20:57:59 -0500

    Test from numpy

--- tests/test-ind2sub.js
+++ tests/test-ind2sub.js
@@ -1,10 +1,10 @@
 const test = require('tape');
-const ind2sub = require('../index');
+const ind2sub = require('../index').ind2sub;
 
 const prod = arr => arr.reduce((acc, n) => acc * n, 1);
 const sum = arr => arr.reduce((acc, n) => acc + n, 0);
 
-test('ind2sub works', (t) => {
+test('ind2sub works', t => {
   const sizes = [ 2, 3, 4 ];
   const totalElements = prod(sizes);
 
@@ -19,3 +19,13 @@ test('ind2sub works', (t) => {
   }
   t.end();
 });
+
+// Via https://docs.scipy.org/doc/numpy/reference/generated/numpy.unravel_index.html
+// >>> np.unravel_index([31, 41, 13], (7,6), order='F')
+// (array([3, 6, 6]), array([4, 5, 1]))
+test('using numpy.unravel_index example', t => {
+  t.deepEqual([ 3, 4 ], ind2sub([ 7, 6 ], 31));
+  t.deepEqual([ 6, 5 ], ind2sub([ 7, 6 ], 41));
+  t.deepEqual([ 6, 1 ], ind2sub([ 7, 6 ], 13));
+  t.end();
+})
